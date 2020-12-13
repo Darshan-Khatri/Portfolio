9bd34c75f49cc1705189047c2f7ae782f51ed90a
Darshan-Khatri <darshankhatri2297@gmail.com>
2020-12-13 03:10:23 -0600

    JavaScript for navigation

---
+++ Script.js
@@ -0,0 +1,27 @@
+
+
+
+const home = document.querySelector('.home');
+const homeNav = document.querySelector('.homeNav');
+const aboutNav = document.querySelector(".aboutNav");
+const about = document.querySelector('.about');
+const project = document.querySelector('.project');
+const projectNav = document.querySelector('.projectNav');
+
+
+aboutNav.addEventListener('click', () => {
+    
+});
+
+homeNav.addEventListener('click', () => {
+    about.classList.toggle("Hide");
+    home.classList.toggle("Hide");
+});
+
+
+var showHide = (className) =>{
+
+    
+
+
+}
