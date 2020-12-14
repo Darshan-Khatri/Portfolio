d2eb35888ae8ae0eb811e4c17c048fdef1eb24b1
Darshan-Khatri <darshankhatri2297@gmail.com>
2020-12-13 18:42:25 -0600

    Navigation

--- Script.js
+++ Script.js
@@ -7,21 +7,38 @@ const aboutNav = document.querySelector(".aboutNav");
 const about = document.querySelector('.about');
 const project = document.querySelector('.project');
 const projectNav = document.querySelector('.projectNav');
+const contact = document.querySelector('.contact');
+const contactNav = document.querySelector('.contactNav');
+
 
 
 aboutNav.addEventListener('click', () => {
-    
+    about.classList.remove("Hide");
+    home.classList.add("Hide");
+    project.classList.add("Hide");
+    contact.classList.add("Hide");
 });
 
 homeNav.addEventListener('click', () => {
-    about.classList.toggle("Hide");
-    home.classList.toggle("Hide");
+    about.classList.add("Hide");
+    home.classList.remove("Hide");
+    project.classList.add("Hide");
+    contact.classList.add("Hide");
 });
 
 
-var showHide = (className) =>{
+projectNav.addEventListener('click', () => {
+    about.classList.add("Hide");
+    home.classList.add("Hide");
+    project.classList.remove("Hide");
+    contact.classList.add("Hide");
+});
 
-    
+contactNav.addEventListener('click', () => {
+    about.classList.add("Hide");
+    home.classList.add("Hide");
+    project.classList.add("Hide");
+    contact.classList.remove("Hide");
+});
 
 
-}
